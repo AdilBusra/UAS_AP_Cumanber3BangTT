a8788788c6b6bd6dcc117b44db7a0c4d45989a78
Adil Busra Muhammad <muhammadadil@students.usu.ac.id>
2025-06-17 16:05:08 +0700

    Add files via upload

--- JUDI.cpp
+++ JUDI.cpp
@@ -42,10 +42,10 @@ class PapanLotreDigital {
 
         void tampilkanPapan() {
             cout << "   ";
-            for (int k = 0; k < jumlah_kolom; ++k) cout << k + 1 << " ";
+            for (int k = 0; k < jumlah_kolom; ++k) cout << k  << " ";
             cout << endl;
             for (int i = 0; i < jumlah_baris; ++i) {
-                cout << i + 1 << "  ";
+                cout << i  << "  ";
                 for (int j = 0; j < jumlah_kolom; ++j) {
                     cout << papan[i][j] << " ";
                 }
@@ -83,40 +83,44 @@ class PapanLotreDigital {
 };
 
 int main() {
-    PapanLotreDigital papan;
-    int baris, kolom;
-    bool selesai = false;
+    char ulang;
+    do {
+        PapanLotreDigital papan;
+        int baris, kolom;
+        bool selesai = false;
 
-    cout << "LOTRE GOSOK DIGITAL\n";
-    cout << "Tebak kotak yang aman. Jika menemukan bom, kamu kalah.\n";
-    cout << "Jika semua kotak aman terbuka, kamu menang!\n\n";
+        cout << "WELCOME TO E-LOTTERY GOSOK\n";
+        cout << "Tebak kotak yang aman. Jika menemukan bom, kamu kalah.\n";
+        cout << "Jika semua kotak aman terbuka, kamu menang!\n";
+        cout << "Mari uji seberapa beruntung dirimu\n\n";
 
-    while (!selesai) {
-        papan.tampilkanPapan();
-        cout << "Masukkan baris dan kolom (contoh: 2 3): ";
-        cin >> baris >> kolom;
-
-        if (cin.fail()) {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Input tidak valid! Masukkan angka.\n";
-            continue;
-        }
-
-        baris -= 1;
-        kolom -= 1;
-
-        bool kena_bom = papan.prosesTebakan(baris, kolom);
-        if (kena_bom) {
-            selesai = true;
-            break;
-        }
-        if (papan.if_menang()) {
+        while (!selesai) {
             papan.tampilkanPapan();
-            cout << "Selamat! Kamu membuka semua kotak aman!\n";
-            selesai = true;
+            cout << "Masukkan baris dan kolom tebakan Anda (contoh: 2 3): ";
+            cin >> baris >> kolom;
+
+            if (cin.fail()) {
+                cin.clear();
+                cin.ignore(10000, '\n');
+                cout << "Input tidak valid! Masukkan angka.\n";
+                continue;
+            }
+
+            bool kena_bom = papan.prosesTebakan(baris, kolom);
+            if (kena_bom) {
+                selesai = true;
+                break;
+            }
+            if (papan.if_menang()) {
+                papan.tampilkanPapan();
+                cout << "Selamat! Kamu membuka semua kotak aman!\n";
+                selesai = true;
+            }
         }
-    }
-    cout << "Permainan selesai.\n";
+        cout << "Permainan selesai.\n";
+        cout << "Apakah ingin bermain lagi? (y/n): ";
+        cin >> ulang;
+        cout << endl;
+    } while (ulang == 'y' || ulang == 'Y');
     return 0;
 }
